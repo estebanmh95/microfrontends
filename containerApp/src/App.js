9967f10106355bac43668d19bdbc692c2c130e20
Esteban Marin <esteban.marinh@crushbank.com>
2022-01-10 16:56:25 -0500

    Add fixed stlye

--- containerApp/src/App.js
+++ containerApp/src/App.js
@@ -3,7 +3,12 @@ import App1 from "./components/App1";
 import App2 from "./components/App2";
 import HeaderMain from "./components/HeaderMain";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'cont'
+})
 export default () => {
     const [showTitle, setShowTitle] = useState(false)
 
@@ -11,14 +16,16 @@ export default () => {
         setShowTitle(!showTitle);
     }
     return(
-        <React.Suspense fallback="Loading header...">         
-            <BrowserRouter>
-                <HeaderMain/>
-                <h1>Main App</h1>
-                <button onClick={()=>setShowTitle(!showTitle)}>Click me</button>
-                {showTitle && <App1/>}
-                {showTitle && <App2/>}
-            </BrowserRouter>   
+        <React.Suspense fallback="Loading header...">  
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>
+                    <HeaderMain/>
+                    <h1>Main App</h1>
+                    <button onClick={()=>setShowTitle(!showTitle)}>Click me</button>
+                    {showTitle && <App1/>}
+                    {showTitle && <App2/>}
+                </BrowserRouter>   
+            </StylesProvider>       
         </React.Suspense>
     )
 }
