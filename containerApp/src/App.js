ca0215dfaa54613e86e759db2d9eddf695d7575c
Esteban Marin <esteban.marinh@crushbank.com>
2022-01-10 16:05:21 -0500

    remove imports

--- containerApp/src/App.js
+++ containerApp/src/App.js
@@ -1,8 +1,9 @@
 import React, { lazy, useState } from "react";
-import App1 from "./components/App1";
-import App2 from "./components/App2";
+// import App1 from "./components/App1";
+// import App2 from "./components/App2";
 import HeaderMain from "./components/HeaderMain";
 import { BrowserRouter } from "react-router-dom";
+
 export default () => {
     const [showTitle, setShowTitle] = useState(false)
 
@@ -15,8 +16,8 @@ export default () => {
                 <HeaderMain/>
                 <h1>Main App</h1>
                 <button onClick={()=>setShowTitle(!showTitle)}>Click me</button>
-                {showTitle && <App1/>}
-                {showTitle && <App2/>}
+                {/* {showTitle && <App1/>}
+                {showTitle && <App2/>} */}
             </BrowserRouter>   
         </React.Suspense>
     )
