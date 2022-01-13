fb0930ce3ec5bd09907fec92748c8629943bb5b6
Esteban Marin <esteban.marinh@crushbank.com>
2022-01-12 21:21:24 -0500

    upload apps again

--- containerApp/src/components/App1.js
+++ containerApp/src/components/App1.js
@@ -1,23 +1,23 @@
-// // import App1 from 'App1/App1'
-// import React,{useRef, useEffect} from 'react';
-// import { useHistory } from 'react-router-dom';
-// import {anotherFunction} from 'App1/App1'
+// import App1 from 'App1/App1'
+import React,{useRef, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import {anotherFunction} from 'App1/App1'
 
-// const App1 = React.lazy(
-//     () => import('App1/App1')
-//   );
-// export default () => {
+const App1 = React.lazy(
+    () => import('App1/App1')
+  );
+export default () => {
 
-//     const history = useHistory();
+    const history = useHistory();
 
-//     const onNavigate = ({pathname: nextPathname}) => {
-//         const {pathname} = history.location
-//         if(pathname !== nextPathname){
-//             history.push(nextPathname)
-//         }
-//     }
-//     const initialPath = history.location.pathname
+    const onNavigate = ({pathname: nextPathname}) => {
+        const {pathname} = history.location
+        if(pathname !== nextPathname){
+            history.push(nextPathname)
+        }
+    }
+    const initialPath = history.location.pathname
 
-//     history.listen(anotherFunction)
-//     return <App1 onNavigate={onNavigate} initialPath={initialPath}/>
-// };
+    history.listen(anotherFunction)
+    return <App1 onNavigate={onNavigate} initialPath={initialPath}/>
+};
