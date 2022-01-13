1f6b8c6279b165137c2b841a2c16d1fcbcc1346b
Esteban Marin <esteban.marinh@crushbank.com>
2022-01-12 21:18:31 -0500

    comment all code

--- containerApp/src/components/Landing.js
+++ containerApp/src/components/Landing.js
@@ -1,12 +1,11 @@
 import './Landing.scss'
 import React from 'react'
 import CardList from './CardList'
-import App1 from './App1'
+// import App1 from './App1'
 
 const Landing = () => {
     return(
         <React.Fragment>
-            {/* <App1/> */}
             <div className='landing--main-container'>
                 <CardList/>
             </div>
