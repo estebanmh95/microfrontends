b62be3b27913d089b60d6e11bbc622aafed8b234
Esteban Marin <esteban.marinh@crushbank.com>
2022-01-12 21:23:32 -0500

    add app2

--- containerApp/src/App.js
+++ containerApp/src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, useState } from "react";
 // import App1 from "./components/App1";
-// import App2 from "./components/App2";
+import App2 from "./components/App2";
 import HeaderMain from "./components/HeaderMain";
 import { BrowserRouter, Route, Switch, Link, Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
@@ -24,8 +24,8 @@ export default () => {
                     {/* <Switch> */}
                         <Route exact path="/"               component={Landing}/>
                         {/* <Route exact path="/pokemon"        component={App1}/>
-                        <Route exact path="/pokemon/:id"    component={App1}/>
-                        <Route exact path="/app2"           component={App2}/> */}
+                        <Route exact path="/pokemon/:id"    component={App1}/> */}
+                        <Route exact path="/app2"           component={App2}/>
                     {/* </Switch> */}
                 </StylesProvider>       
             </React.Suspense>
